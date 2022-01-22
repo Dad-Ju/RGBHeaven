a6f95c7373c16057c824972e096d848af31c0bd7
Dad-Ju <Justin.Bedrich@gmail.com>
2022-01-22 20:10:58 +0100

    try other led lib

--- client/index.js
+++ client/index.js
@@ -1,4 +1,25 @@
 const io = require('socket.io')()
+// eslint-disable-next-line import/no-unresolved
+const LedDriver = require('rpi-ws281x-led')
+// eslint-disable-next-line import/no-unresolved
+const { StripType } = require('rpi-ws281x-led')
+const config = require('./config.json')
+
+const ledDriver = new LedDriver({
+	channels: [
+		{
+			gpio: config.stripes[0].Pin,
+			count: config.stripes[0].LEDS,
+			type: StripType[`${config.stripes[0].type}_STRIP`],
+			brightness: 30,
+		},
+	],
+})
+
+const stripe = ledDriver.channels[0]
+
+stripe.leds = new Uint32Array(100).fill(0x000000)
+stripe.render()
 
 io.on('connection', (client) => {
 	console.log(client)
