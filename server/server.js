18802d3e7ea36c525361e14bdcfc900e4e1ac2f8
Dad-Ju <Justin.Bedrich@gmail.com>
2022-01-23 18:34:31 +0100

    fix dump error

--- server/server.js
+++ server/server.js
@@ -2,14 +2,12 @@ const express = require('express')
 const app = require('express')()
 const server = require('http').createServer(app)
 const io = require('socket.io')(server)
-const client = require('socket.io-client')
+const rpi = require('socket.io-client')('http://localhost:3001')
 
 app.use(express.static('public'))
 
-const rpi = client('http://localhost:3001')
-
-io.on('connection', () => {
-	io.on('frame', (data) => {
+io.on('connection', (client) => {
+	client.on('frame', (data) => {
 		console.log(`Recived: ${data}, Sending it to RPI now!`)
 		rpi.emit('frame', data)
 	})
