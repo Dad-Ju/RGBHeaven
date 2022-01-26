9d7cb39432c7fcb7659a84fd3dedfc037e3072cc
Dad-Ju <Justin.Bedrich@gmail.com>
2022-01-26 22:16:00 +0100

    bug?

---
+++ dashboard-old/snowpack.config.mjs
@@ -0,0 +1,28 @@
+/** @type {import("snowpack").SnowpackUserConfig } */
+const config = {
+	mount: {
+		public: '/public',
+		src: '/',
+	},
+	plugins: ['@snowpack/plugin-react-refresh'],
+	routes: [
+		/* Enable an SPA Fallback in development: */
+		// {"match": "routes", "src": ".*", "dest": "/index.html"},
+	],
+	optimize: {
+		/* Example: Bundle your final build: */
+		// "bundle": true,
+	},
+	packageOptions: {
+		/* ... */
+	},
+	devOptions: {
+		/* ... */
+	},
+	buildOptions: {
+		out: '../server/public',
+		clean: true,
+	},
+}
+
+export default config
