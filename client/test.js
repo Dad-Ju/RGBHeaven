7ad2725dab50ab912c14791c73b97b9bd91f9331
Dad-Ju <Justin.Bedrich@gmail.com>
2022-02-05 22:49:59 +0100

    dafuq ?

--- client/test.js
+++ client/test.js
@@ -1,19 +1,14 @@
 // eslint-disable-next-line import/no-extraneous-dependencies
 const { Gpio } = require('pigpio')
 
-// Set pin to High
-new Gpio(2, { mode: Gpio.OUTPUT }).digitalWrite(1)
-console.log('VCC High!')
-
 const button = new Gpio(0, {
 	mode: Gpio.INPUT,
-	pullUpDown: Gpio.PUD_DOWN,
-	alert: true,
+	pullUpDown: Gpio.FALLING_EDGE,
 })
 
 // Level must be stable for 10 ms before an alert event is emitted.
-button.glitchFilter(10000)
+// button.glitchFilter(10000)
 
-button.on('alert', (level) => {
+button.on('interrupt', (level) => {
 	console.log(level)
 })
