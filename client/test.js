04278bce9c74d2387d505792b4eea98269c97187
Dad-Ju <Justin.Bedrich@gmail.com>
2022-02-05 22:09:43 +0100

    yeet

--- client/test.js
+++ client/test.js
@@ -1,8 +1,7 @@
 // eslint-disable-next-line import/no-extraneous-dependencies
 const gpiop = require('rpi-gpio').promise
 
-// eslint-disable-next-line import/newline-after-import
-await gpiop.setup(0, gpiop.DIR_IN)
+gpiop.setup(0, gpiop.DIR_IN)
 
 gpiop.on('export', (value) => {
 	console.log('Yeet', value)
