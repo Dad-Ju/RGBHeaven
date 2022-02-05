0d97cc9de41ed35bdcd554f65d885fdb86b1a00b
Dad-Ju <Justin.Bedrich@gmail.com>
2022-02-05 23:29:37 +0100

    yeet

--- client/test.js
+++ client/test.js
@@ -1,19 +1,16 @@
 // eslint-disable-next-line import/no-extraneous-dependencies
 const { Gpio } = require('pigpio')
 
-const button = new Gpio(0, {
+const button = new Gpio(17, {
 	mode: Gpio.INPUT,
 	pullUpDown: Gpio.PUD_DOWN,
 	edge: Gpio.EITHER_EDGE,
 	// timeout: null,
 })
 
-while (true) {
-	console.log(button.digitalRead())
-}
 // Level must be stable for 10 ms before an alert event is emitted.
-// button.glitchFilter(10000)
+button.glitchFilter(10000)
 
-// button.on('interrupt', (level) => {
-// 	console.log(level)
-// })
+button.on('interrupt', (level) => {
+	console.log(level)
+})
