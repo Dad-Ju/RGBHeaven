64cdf17b01c23f46dd57c1eb7206d120e8538a0b
Dad-Ju <Justin.Bedrich@gmail.com>
2022-02-06 00:32:38 +0100

    dont have enougth wire to test on breadboard

--- client/test.js
+++ client/test.js
@@ -1,15 +1,53 @@
 // eslint-disable-next-line import/no-extraneous-dependencies
 const { Gpio } = require('pigpio')
 
-const button = new Gpio(17, {
+let vccState = false
+let lightSwitchState = false
+let relayState = false
+
+const lightSwitch = new Gpio(17, {
 	mode: Gpio.INPUT,
 	pullUpDown: Gpio.PUD_UP,
 	alert: true,
 })
 
-// Level must be stable for 10 ms before an alert event is emitted.
-button.glitchFilter(1000)
+const vcc = new Gpio(22, {
+	mode: Gpio.INPUT,
+	pullUpDown: Gpio.PUD_UP,
+	alert: true,
+})
 
-button.on('alert', (level) => {
-	console.log(level)
+const relay = new Gpio(27, {
+	mode: Gpio.OUTPUT,
+	alert: true,
+})
+
+relay.digitalWrite(0)
+
+const checkForStatus = () => {
+	if (lightSwitchState && !relayState) {
+		relay.digitalWrite(1)
+		return
+	}
+	if (!vccState && !lightSwitchState) {
+		relay.digitalWrite(0)
+	}
+}
+// Level must be stable for 10 ms before an alert event is emitted.
+lightSwitch.glitchFilter(1000)
+vcc.glitchFilter(1000)
+vcc.glitchFilter(1000)
+
+lightSwitch.on('alert', (level) => {
+	lightSwitchState = !level
+	checkForStatus()
+})
+
+vcc.on('alert', (level) => {
+	vccState = !level
+	checkForStatus()
+})
+
+relay.on('alert', (level) => {
+	relayState = Boolean(level)
 })
